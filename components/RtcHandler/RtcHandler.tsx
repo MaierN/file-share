3c542c5bcd621348f70d7e9b4d4fcb6deb77a57b
Nicolas Maier <20108526+MaierN@users.noreply.github.com>
2023-02-02 15:08:48 +0100

    added turn servers

--- components/RtcHandler/RtcHandler.tsx
+++ components/RtcHandler/RtcHandler.tsx
@@ -73,6 +73,22 @@ function RtcHandler({ children }: { children: ReactNode }) {
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
         { urls: "stun:stun.stunprotocol.org:3478" },
+        { urls: "stun:relay.metered.ca:80" },
+        {
+          urls: "turn:relay.metered.ca:80",
+          username: "8abe596f7037054963790fd7",
+          credential: "kWTnzEsD2UHkG+Wm",
+        },
+        {
+          urls: "turn:relay.metered.ca:443",
+          username: "8abe596f7037054963790fd7",
+          credential: "kWTnzEsD2UHkG+Wm",
+        },
+        {
+          urls: "turn:relay.metered.ca:443?transport=tcp",
+          username: "8abe596f7037054963790fd7",
+          credential: "kWTnzEsD2UHkG+Wm",
+        },
       ],
     });
   });
